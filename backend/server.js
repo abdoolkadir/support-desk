89c8b4884ae3aa8a04646cc3341203bdffcd3dcf
Abdulkadir Adamu <adamuhammadabdulkadir@gmail.com>
2022-06-03 13:50:14 +0100

    routes controller and error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log('Server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Create route
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support Desk API' });
+});
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
