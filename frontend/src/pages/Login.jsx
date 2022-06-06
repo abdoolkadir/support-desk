d9635eba96cd6e90c2ad70216323d797f624f8ed
Abdulkadir Adamu <adamuhammadabdulkadir@gmail.com>
2022-06-06 23:54:45 +0100

    hooked up login and register forms redux

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { FaSignInAlt } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { login } from '../features/auth/authSlice';
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -8,7 +10,15 @@ function Login() {
     password: ''
   });
 
-  const { name, email, password, password2 } = formData;
+  const { email, password } = formData;
+
+  // init dispatch
+  const dispatch = useDispatch();
+
+  // get state
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -19,6 +29,13 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    const userData = {
+      email,
+      password
+    };
+
+    dispatch(login(userData));
   };
 
   return (
